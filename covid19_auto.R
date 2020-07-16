ce421d13e23419ccbd95ead4e1aad9d81ec33fbf
David Chiu <tr.ywchiu@gmail.com>
2020-07-16 11:39:13 +0800

    covid19 data auto download

---
+++ covid19_auto.R
@@ -0,0 +1,47 @@
+library(lubridate)
+library(logging)
+
+# Downloading Script
+download_covi19 <- function(start_date, end_date){
+  for(i in seq(start_date, end_date, by= 'days')){
+    dt      <- as_date(i)
+    dt_str  <- format(dt, format="%m-%d-%Y")
+    url_str <- sprintf(url, dt_str)
+    filename <- sprintf('%s.csv', dt_str)
+    if (! file.exists(filename)){
+      tryCatch({
+        loginfo(url_str, logger="event")
+        download.file(url_str, filename)
+      }, error = function(err) {
+        logerror(url_str, logger="event")
+        #print(url_str)
+      })
+    }
+  }
+}
+
+
+
+
+url <- 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/%s.csv'
+
+# Handling Logging
+basicConfig()
+addHandler(writeToFile, logger="event", file="covid19_event.log")
+
+
+# Handling Arguments
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args)==0) {
+  end_date   <- today(tz="Asia/Taipei")
+  start_date <- end_date - days(3)
+  download_covi19(start_date, end_date)
+} else if (length(args)==2) {
+  start_date <- ymd(args[1])
+  end_date   <- ymd(args[2])
+  download_covi19(start_date, end_date)
+} else{
+  stop("Please input 0 argument or 2 arguments", call.=FALSE)
+}
+
