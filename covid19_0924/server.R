56d0d1c4966ccd06cb5e740688a94c7f4c516384
“david“ <david@largitdata.com>
2020-09-24 10:50:11 +0800

    add covid19_0924

--- covid19_0924/server.R
+++ covid19_0924/server.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(tidyverse)
 library(leaflet)
+library(plotly)
 
 shinyServer(function(input, output, session) {
     covid19 <- read_csv('covid19.csv')
@@ -9,7 +10,7 @@ shinyServer(function(input, output, session) {
     output$covidmap <- renderLeaflet({
 
         confirmed <- covid19 %>%
-            filter(`Case` == 'confirmed')
+            filter(`Case` == input$case_type, Date == input$timeSlider)
 
         map <- leaflet(confirmed) %>%
             addTiles() %>%
@@ -18,10 +19,61 @@ shinyServer(function(input, output, session) {
                 lng = confirmed$Long,
                 radius = log(confirmed$Case_Number),
                 stroke = FALSE,
-                label=as.character(paste(confirmed$`Country/Region`, confirmed$`Case_Number`)),
-                popup = as.character(confirmed$`Country/Region`),
+                color = ifelse(confirmed$Case_Number > 1000000, 'red', 'blue'),
+                label=as.character(paste(confirmed$`Country/Region`, formatC(confirmed$Case_Number, format="f", big.mark=",", digits=0))),
+                popup = as.character(paste(confirmed$`Country/Region`, formatC(confirmed$Case_Number, format="f", big.mark=",", digits=0))),
                 fillOpacity = 0.5
             )
         map
+    })
+
+    output$confirmed_all <- renderValueBox({
+        case_stat <-  covid19 %>%
+            filter(Case == 'confirmed', Date == input$timeSlider)
+        valueBox(
+            formatC(sum(case_stat$Case_Number), format="f", big.mark=",", digits=0),
+            color="orange",
+            "確診人數",
+            icon = icon("procedures")
+        )
+    })
+    output$deaths_all <- renderValueBox({
+        case_stat <-  covid19 %>%
+            filter(Case == 'deaths', Date == input$timeSlider)
+        valueBox(
+            formatC(sum(case_stat$Case_Number), format="f", big.mark=",", digits=0),
+            color="red",
+            "死亡人數",
+            icon = icon("skull-crossbones")
+        )
+    })
+    output$recovered_all <- renderValueBox({
+        case_stat <-  covid19 %>%
+            filter(Case == 'recovered', Date == input$timeSlider)
+        valueBox(
+            formatC(sum(case_stat$Case_Number), format="f", big.mark=",", digits=0),
+            color="green",
+            "康復人數",
+            icon = icon("first-aid")
+        )
+    })
+    output$case_ranking <- renderPlotly({
+        case_stat <- covid19 %>%
+            filter(Case == input$case_type, Date == input$timeSlider) %>%
+            group_by(`Country/Region`) %>%
+            summarise(Case_Number = sum(Case_Number)) %>%
+            arrange(desc(Case_Number)) %>%
+            head(10)
+
+        fig <- ggplot(case_stat)
+
+        g <- fig +
+            geom_bar(aes(x=reorder(`Country/Region`, Case_Number),y=Case_Number),stat="identity", fill= 'blue') +
+            coord_flip() +
+            ylab('Case Numbers') +
+            xlab('Country')
+
+        ggplotly(g)
+
     })
 })
--- covid19_0924/ui.R
+++ covid19_0924/ui.R
@@ -1,10 +1,15 @@
 library(shiny)
 library(shinydashboard)
 library(leaflet)
-
+library(plotly)
 header  <- dashboardHeader(title = "新冠肺炎儀表板")
 
 body <- dashboardBody(
+    fluidRow(
+        valueBoxOutput('confirmed_all'),
+        valueBoxOutput('deaths_all'),
+        valueBoxOutput('recovered_all')
+    ),
     fluidRow(
         column(width = 9,
             box(width = NULL, solidHeader = TRUE,
@@ -13,16 +18,35 @@ body <- dashboardBody(
         ),
         column(width = 3,
              box(width = NULL, solidHeader = TRUE,
-                   #leafletOutput("covidmap", height = 500)
+                 radioButtons("case_type",
+                              label = "個案類型排行",
+                              choices = list("確診" = "confirmed", "死亡" = "deaths", "康復" = "recovered"),
+                              selected = "confirmed",
+                              inline=TRUE)
+
+             ),
+             box(width = NULL, solidHeader = TRUE,
+                 plotlyOutput("case_ranking")
              )
         )
     ),
     fluidRow(
-        column(width = 12,
-               box(width = NULL, solidHeader = TRUE,
-                   #leafletOutput("covidmap", height = 500)
-               )
+        column(
+            sliderInput(
+                "timeSlider",
+                label  = "Select date",
+                min    = as.Date('2020-01-22'),
+                max    = as.Date('2020-09-22'),
+                value  = as.Date('2020-09-22'),
+                width  = "100%",
+                timeFormat = "%Y/%m/%d",
+                animate  = animationOptions(loop = FALSE)
+            ),
+            class = "slider",
+            width = 12,
+            style = 'padding-left:15px; padding-right:15px;'
         )
+
     )
 )
 
