46c8d7dfe6c2e362535a53e0bd7d0d11a83a6a90
“david“ <david@largitdata.com>
2020-10-14 10:13:12 +0800

    add 2020 exam

--- 2020Exam/server.R
+++ 2020Exam/server.R
@@ -4,24 +4,22 @@ library(plotly)
 library(lubridate)
 
 shinyServer(function(input, output, session) {
-    covid <- read_csv('')
-
-    observeEvent(input$Case_Type, {
-        # TODO
-    })
+    covid <- read_csv('https://raw.githubusercontent.com/ywchiu/cdc_course/master/2020Exam/covid19.csv')
 
     output$accumPlot <- renderPlotly({
-        # TODO
+        # 題目一：請根據輸入個案類型與輸入國家產生累積個案折線趨勢圖(X 軸為時間，Y 為累積個案數量)
     })
 
     output$dailyNewPlot <- renderPlotly({
-        # TODO
+        # 題目二：請根據輸入個案類型與輸入國家產生每日新增個案長條圖(X 軸為時間，Y 為每日個案增加數量)
     })
 
     output$table <- renderDataTable({
-        # TODO
+        # 題目三：請根據輸入個案類型與輸入國家產生每日累積個案表格(至少包含欄位：日期、國家、累積個案數量)，表格必須根據日期排序(由新至舊)
     })
 
-
+    observeEvent(input$Case_Type, {
+        # 題目四：請產生所有國家組合，讓Shiny 介面選單上(輸入國家)可以選擇所有國家
+    })
 
 })
